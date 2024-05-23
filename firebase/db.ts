7bd32985aee0cfc9627c985d9cae3455c8925001
david.robby.sanjaya <david.robby.sanjaya@gmail.com>
2024-05-23 17:41:48 +0000

    hide credential

--- firebase/db.ts
+++ firebase/db.ts
@@ -1,16 +1,17 @@
 import {  getApps, initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import 'dotenv/config';
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyD8-RA37YyHhDsVhbGn1mp1xBrI7N0D4U8",
-    authDomain: "cu-monitoring-d0c7c.firebaseapp.com",
-    projectId: "cu-monitoring-d0c7c",
-    storageBucket: "cu-monitoring-d0c7c.appspot.com",
-    messagingSenderId: "671304905280",
-    appId: "1:671304905280:web:d29d739e5907de0b35a5a5",
-    measurementId: "G-ZLJLCCCBFR"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MSG_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
   
 };
 
